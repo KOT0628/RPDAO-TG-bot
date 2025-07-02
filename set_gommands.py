0cf75857427cf9240885709cb5862f92aca2e599
ⓀⓄⓉ <36645434+KOT0628@users.noreply.github.com>
2025-07-02 06:08:18 +0300

    Update set_gommands.py

--- set_gommands.py
+++ set_gommands.py
@@ -8,12 +8,17 @@ url = f"https://api.telegram.org/bot{TOKEN}/setMyCommands"
 
 commands = [
     {"command": "price", "description": "Show current price $BTC"},
-    {"command": "start_roll", "description": "Start ROLL (only admins)"},
     {"command": "roll", "description": "ROLL"},
     {"command": "reroll", "description": "Throw the dice"},
     {"command": "score", "description": "Leaderboard"},
     {"command": "gm", "description": "Good morning RPDAO"},
     {"command": "gn", "description": "Good night RPDAO"},
+    {"command": "start_roll", "description": "Start ROLL (only admins)"},
+    {"command": "rpdao_trivia", "description": "Launch of TRIVIA (only admins)"},
+    {"command": "reroll_on", "description": "Launch of Throw the dice (only admins)"},
+    {"command": "stop_roll", "description": "Stop ROLL (only admins)"},
+    {"command": "rpdao_trivia_off", "description": "Stop TRIVIA (only admins)"},
+    {"command": "reroll_off", "description": "Stop Throw the dice (only admins)"},
     {"command": "start", "description": "Restart RPDAO Harvester"},
 ]
 
